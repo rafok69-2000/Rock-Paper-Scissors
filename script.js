de420347a67be95f90c7529d00995b3f5119f84f
Rafael Santos Ciriaco <santosrafael9279@gmail.com>
2026-02-10 14:21:58 -0600

    Create playRound() function
    Create playRound() and constants they call getHumanChoice and
    getComputerChoice functions

--- script.js
+++ script.js
@@ -8,14 +8,56 @@ function getComputerChoice(){
   }else if (option == 2){
     return "paper"
   }else if (option == 3)
-    return "scissors"
+    return "scissor"
 }
-
-console.log(getComputerChoice());
 
 function getHumanChoice(){
    let optionHuman = prompt("Rock, paper or scissor");
    return optionHuman
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    console.log("The computer says: " + computerChoice);
+    console.log("The human says: " + humanChoice);
+    if (humanChoice == computerChoice){
+        console.log("Dead Heat!");
+    }else{
+    switch (humanChoice){
+        case "rock":
+            if(computerChoice == "paper"){
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+            }else if(computerChoice == "scissor"){
+                console.log("You win! Rock beats Scissor");
+                humanScore++;
+            }
+        break; 
+        
+        case "paper":
+            if(computerChoice == "scissor"){
+                console.log("You lose! Scissor beats paper");
+                computerChoice++;
+            }else{
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+            }
+        break;
+
+        case "scissor":
+            if(computerChoice = "rock"){
+                console.log("You lose! Rock beats Scissor");
+                computerChoice++;
+            }else{
+                console.log("You win! Scissor beats Paper");
+                humanScore++;
+            }
+    }
+}
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
