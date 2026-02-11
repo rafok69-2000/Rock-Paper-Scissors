cbd33fd74396a63a5688189bfbc230f402dc225d
Rafael Santos Ciriaco <santosrafael9279@gmail.com>
2026-02-10 21:32:55 -0600

    Create playGame() function
    Create the game logic that allows the user to play 5 times

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice(){
   let option = Math.floor(Math.random() * (3 - 1 + 1) + 1);
@@ -16,48 +14,65 @@ function getHumanChoice(){
    return optionHuman
 }
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-    console.log("The computer says: " + computerChoice);
-    console.log("The human says: " + humanChoice);
-    if (humanChoice == computerChoice){
-        console.log("Dead Heat!");
-    }else{
-    switch (humanChoice){
-        case "rock":
-            if(computerChoice == "paper"){
-                console.log("You lose! Paper beats Rock");
-                computerScore++;
-            }else if(computerChoice == "scissor"){
-                console.log("You win! Rock beats Scissor");
-                humanScore++;
-            }
-        break; 
-        
-        case "paper":
-            if(computerChoice == "scissor"){
-                console.log("You lose! Scissor beats paper");
-                computerChoice++;
-            }else{
-                console.log("You win! Paper beats Rock");
-                humanScore++;
-            }
-        break;
+let humanScore = 0;
+let computerScore = 0;
 
-        case "scissor":
-            if(computerChoice = "rock"){
-                console.log("You lose! Rock beats Scissor");
-                computerChoice++;
+
+function playGame(){
+
+    for(i = 0; i < 5; i++){
+
+        function playRound(humanChoice, computerChoice){
+            humanChoice = humanChoice.toLowerCase();
+            console.log("The computer says: " + computerChoice);
+            console.log("The human says: " + humanChoice);
+            if (humanChoice == computerChoice){
+                console.log("Dead Heat!");
+                console.log("Computer: " + computerScore);
+                console.log("Human " + humanScore);
             }else{
-                console.log("You win! Scissor beats Paper");
-                humanScore++;
+                switch (humanChoice){
+                    case "rock":
+                        if(computerChoice == "paper"){
+                            console.log("You lose! Paper beats Rock");
+                            computerScore++;
+                        }else if(computerChoice == "scissor"){
+                            console.log("You win! Rock beats Scissor");
+                            humanScore++;
+                        }
+                    break; 
+                    
+                    case "paper":
+                        if(computerChoice == "scissor"){
+                            console.log("You lose! Scissor beats paper");
+                            computerChoice++;
+                        }else{
+                            console.log("You win! Paper beats Rock");
+                            humanScore++;
+                        }
+                    break;
+
+                    case "scissor":
+                        if(computerChoice = "rock"){
+                            console.log("You lose! Rock beats Scissor");
+                            computerChoice++;
+                        }else{
+                            console.log("You win! Scissor beats Paper");
+                            humanScore++;
+                        }
+                }
+                console.log("Computer: " + computerScore);
+                console.log("Human " + humanScore);
             }
+
+        }   
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+ 
     }
 }
 
-}
+playGame();
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
