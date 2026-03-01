8eb962a5aca09c72734d35574014596afb3881c4
Rafael Santos Ciriaco <santosrafael9279@gmail.com>
2026-03-01 13:30:11 -0600

    Repleace logic in the playRound function

--- script.js
+++ script.js
@@ -29,50 +29,30 @@ let computerScore = 0;
 
 
 
-        function playRound(humanChoice, computerChoice){
-            humanChoice = humanChoice.toLowerCase();
-            console.log("The computer says: " + computerChoice);
-            console.log("The human says: " + humanChoice);
-            if (humanChoice == computerChoice){
-                console.log("Dead Heat!");
-                console.log("Computer: " + computerScore);
-                console.log("Human " + humanScore);
-            }else{
-                switch (humanChoice){
-                    case "rock":
-                        if(computerChoice == "paper"){
-                            console.log("You lose! Paper beats Rock");
-                            computerScore++;
-                        }else if(computerChoice == "scissor"){
-                            console.log("You win! Rock beats Scissor");
-                            humanScore++;
-                        }
-                    break; 
-                    
-                    case "paper":
-                        if(computerChoice == "scissor"){
-                            console.log("You lose! Scissor beats paper");
-                            computerScore++;
-                        }else{
-                            console.log("You win! Paper beats Rock");
-                            humanScore++;
-                        }
-                    break;
+function playRound(humanChoice, computerChoice) {
 
-                    case "scissor":
-                        if(computerChoice == "rock"){
-                            console.log("You lose! Rock beats Scissor");
-                            computerScore++;
-                        }else{
-                            console.log("You win! Scissor beats Paper");
-                            humanScore++;
-                        }
-                }
-                console.log("Computer: " + computerScore);
-                console.log("Human " + humanScore);
-            }
+    humanChoice = humanChoice.toLowerCase();
 
-        }   
+    console.log(`Computer says: ${computerChoice}`);
+    console.log(`Human says: ${humanChoice}`);
+
+    if (humanChoice === computerChoice) {
+        console.log("Dead Heat!");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    console.log(`Computer: ${computerScore}`);
+    console.log(`Human: ${humanScore}`);
+}   
         
  
     
